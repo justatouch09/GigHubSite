7e4a131f222a87ad0859e15463f4125e50af0b99
justatouch09 <justatouch09@gmail.com>
2017-11-04 17:18:31 -0400

    Implement validation.

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,6 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -33,10 +32,17 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
+
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                Datetime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Datetime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
